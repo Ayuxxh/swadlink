ca52f68177180c81da4c15b7a4a67fef04523acd
Ayuxxh <jadhavayush1100@gmail.com>
2025-08-09 03:03:47 +0530

    setup for production

--- swadlink/swadlink/settings.py
+++ swadlink/swadlink/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 
 from pathlib import Path
 import psycopg
+from decouple import config
 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -22,15 +23,15 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-o!!(gk9^fdfztib#k*5rj!nmf9fu4lts^-w4(fp%5xszbbng0g'
+SECRET_KEY = config("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config("DEBUG", cast=bool, default=False)
 
 ALLOWED_HOSTS = ['*']
 
 
-
+    
 # Application definition
 
 INSTALLED_APPS = [
@@ -86,10 +87,10 @@ WSGI_APPLICATION = 'swadlink.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'qrahi_database',
-        'USER': 'qrahi_database_user',
-        'PASSWORD': 'uMBa6gGfyWCUz7CNgfVMlMwaJz78cjRu',
-        'HOST': 'dpg-d2a6nb1r0fns739202n0-a.singapore-postgres.render.com',
+        'NAME': config("DB_NAME"),
+        'USER': config('DB_USER'),
+        'PASSWORD': config('DB_PASSWORD'),
+        'HOST': config('DB_HOST'),
         'PORT': '5432',
          'OPTIONS': {
                  'sslmode': 'require',
