ebb4c87ea44ff1bbec9ef81c6b3671ec108c1933
Ayuxxh <jadhavayush1100@gmail.com>
2026-02-24 23:08:24 +0530

    Qr MEnu setup

--- swadlink/swadlink/settings.py
+++ swadlink/swadlink/settings.py
@@ -88,15 +88,11 @@ WSGI_APPLICATION = 'swadlink.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': config("DB_NAME"),
-        'USER': config('DB_USER'),
-        'PASSWORD': config('DB_PASSWORD'),
-        'HOST': config('DB_HOST'),
+        'NAME': 'postgres',
+        'USER': 'postgres',
+        'PASSWORD': 'Tanishka@ayus1',
+        'HOST': 'db.iplmcpsoarymjbsnauyv.supabase.co',
         'PORT': '5432',
-         'OPTIONS': {
-                 'sslmode': 'require',
-            'client_encoding': 'UTF8',
-        },
     }
 }
 
